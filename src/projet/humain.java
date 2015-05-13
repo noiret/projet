8edf616d0f4b35b9cad529942557aa7693e9e600
unknown <ravenet-geoffrey@hotmail.fr>
2015-05-13 16:54:28 +0200

    ajout des source

---
+++ src/projet/humain.java
@@ -0,0 +1,15 @@
+package projet;
+
+public class humain {
+	private String nom;
+	private String boissonFavorite;
+	
+	public humain(String nom,String boissonFavorite){
+		this.nom="nom";
+		this.boissonFavorite="eau";
+	}
+	
+	public static void parle(String unnom,String uneboissonFavorite){
+		System.out.println(unnom+" - Ah un bon verre de "+uneboissonFavorite+" ! GLOUPS !");
+	}
+}
